a1583468720942b7f1db9e0aa4c826d866cf67a8
Miguel-Colmenares <sebastianyt626@gmail.com>
2026-04-06 16:45:11 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/Delissa/Delissa/DelissaApplication.java
@@ -0,0 +1,13 @@
+package com.Delissa.Delissa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelissaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelissaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Delissa/Delissa/DelissaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Delissa.Delissa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelissaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
